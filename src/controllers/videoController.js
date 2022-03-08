90cf552654190b5e3d8b6caef5040feb57663d51
jinhyeok0204 <aqwstn@naver.com>
2022-03-08 23:33:31 +0900

    add views

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,10 @@
 // for globalRouter
-export const recommend = (req, res) => res.send("Recommended Videos");
+export const recommend = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search Videos");
 // for videoRouter
 export const seeVideo = (req, res) => {
-    console.log(req.params);
-    return res.send("Watch Videos");
+    res.render("seevideo");
 };
-export const editVideo = (req, res) => res.send("Edit Videos");
+export const editVideo = (req, res) => res.render("editvideo");
 export const uploadVideo = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -9,13 +9,15 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-app.use(logger);
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+    console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
