5e4ea6e0dcf129e2568b3599fa5010cd7420999c
Jinhyeok Jeon <aqwstn@naver.com>
2022-04-16 17:38:20 +0900

    fix miss spelling

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -45,4 +45,4 @@ export const login = (req, res) => res.send("Login Page");
 export const seeProfile = (req, res) => res.send("Profile");
 export const logout = (req, res) => res.send("Logout");
 export const editUser = (req, res) => res.send("Edit User");
-export const deleteUser = (req, res) => res.send("Delte User");
+export const deleteUser = (req, res) => res.send("Delete User");
